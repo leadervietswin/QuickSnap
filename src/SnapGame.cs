280d3dba6974bfad329b515b61df1370f257716c
leadervietswin <leadervietswin@protonmail.club>
2017-03-21 00:06:56 +1100

    add sound effects for game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -12,6 +12,9 @@ namespace CardGames
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
 			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
+			SwinGame.LoadSoundEffectNamed ("Slap", "slap.wav");
+			SwinGame.LoadSoundEffectNamed ("Start", "SwinGameStart.wav");
+			SwinGame.LoadSoundEffectNamed ("Error", "error.wav");
 		}
 
 		/// <summary>
@@ -26,6 +29,7 @@ namespace CardGames
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
 			{
 				myGame.Start ();
+				SwinGame.PlaySoundEffect ("Start");
 			}
 			
 			if (myGame.IsStarted)
@@ -33,15 +37,17 @@ namespace CardGames
 				if ( SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
 				SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
 				{
-					
+					SwinGame.PlaySoundEffect ("Error");
 				}
 				else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT))
 				{
 					myGame.PlayerHit (0);
+					SwinGame.PlaySoundEffect ("Slap");
 				}
 				else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
 				{
 					myGame.PlayerHit (1);
+					SwinGame.PlaySoundEffect ("Slap");
 				}
 			}
 		}
